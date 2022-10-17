82ea855d2fd049fa3737a9f1ce983b3cc00f22e0
Darsh Patel <pareldarshn119@gmail.com>
2022-10-17 14:57:48 -0400

    Initial Commit

---
+++ APPLEPIE_FINAL1UITests/APPLEPIE_FINAL1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  APPLEPIE_FINAL1UITestsLaunchTests.swift
+//  APPLEPIE_FINAL1UITests
+//
+//  Created by Darsh Patel on 10/17/22.
+//
+
+import XCTest
+
+final class APPLEPIE_FINAL1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
